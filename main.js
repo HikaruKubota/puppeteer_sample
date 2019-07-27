a43b17e096088bb9b590c79463aa2335f6d17150
kubota <sakaku193@gmail.com>
2019-07-27 14:51:48 +0900

    一通りほしそうな情報の取得が完了

--- main.js
+++ main.js
@@ -16,14 +16,19 @@ const puppeteer = require('puppeteer');
   for (const list of lists) {
     const title = await list.$('.summary > h3');
     const href = await title.$('a');
+    const entry_data = await list.$('.entry > .entry_data_row > .entry_data > .inner_cell');
+    const payment_label  = await entry_data.$('.payment_label');
+    const amount  = await entry_data.$('.amount');
+
     var data = {
       title: await (await title.getProperty('textContent')).jsonValue(),
       href: await (await href.getProperty('href')).jsonValue(),
+      payment_label: await (await payment_label.getProperty('textContent')).jsonValue(),
+      amount: await (await amount.getProperty('textContent')).jsonValue(),
     };
     datas.push(data);
   }
   console.log(datas);
-
 
   await browser.close();
 })();
