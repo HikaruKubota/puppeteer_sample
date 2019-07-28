b9d5ea92c7be3dbeb4da8d27a314b754a44c4690
AIちゃん <sakaku193@gmail.com>
2019-07-28 10:34:56 +0900

    色々！！！

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const my_module = require('./my_module');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -8,29 +9,30 @@ const puppeteer = require('puppeteer');
   });
   //クラウドワークス 新着 システム開発
   await page.goto('https://crowdworks.jp/public/jobs/search?category_id=226&keep_search_criteria=true&order=new&hide_expired=true');
+
+  let items = await page.$$('.item_body');
+  let datas = await my_module.create_datas(items);
+
+  const nowDate = new Date();
+  console.log(
+    datas.filter(date => {
+      const DatPublished = date.DatPublished.replace(/\s+/g, "").split('：')[1];
+      return nowDate.getDate() == DatPublished.slice(8,10);
+    })
+  );
+
   //クラウドワークス 新着 アプリ・スマートフォン開発
-  // await page.goto('https://crowdworks.jp/public/jobs/search?category_id=242&keep_search_criteria=true&order=new&hide_expired=true');
+  await page.goto('https://crowdworks.jp/public/jobs/search?category_id=242&keep_search_criteria=true&order=new&hide_expired=true');
 
-  var items = await page.$$('.item_body');
-  var datas = [];
-  for (const item of items) {
-    const title = await item.$('.job_data_row > .summary > h3');
-    const href = await title.$('a');
-    const entry_data = await item.$('.job_data_row > .entry > .entry_data_row > .entry_data > .inner_cell');
-    const payment_label = await entry_data.$('.payment_label');
-    const amount = await entry_data.$('.amount');
-    const DatPublished = await item.$('.item_meta > .post_date');
+  items = await page.$$('.item_body');
+  datas = await my_module.create_datas(items);
 
-    var data = {
-      title: await (await title.getProperty('textContent')).jsonValue(),
-      href: await (await href.getProperty('href')).jsonValue(),
-      payment_label: await (await payment_label.getProperty('textContent')).jsonValue(),
-      amount: await (await amount.getProperty('textContent')).jsonValue(),
-      DatPublished: await (await DatPublished.getProperty('textContent')).jsonValue(),
-    };
-    datas.push(data);
-  }
-  console.log(datas);
+  console.log(
+    datas.filter(date => {
+      const DatPublished = date.DatPublished.replace(/\s+/g, "").split('：')[1];
+      return nowDate.getDate() == DatPublished.slice(8,10);
+    })
+  );
 
   await browser.close();
 })();
