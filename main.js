da368730dab3f25a44dbd874b39c36dd91a26251
AIちゃん <sakaku193@gmail.com>
2019-07-29 00:26:01 +0900

    必要なコメントと必要な記述を追加

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ const my_slack = require('./slack');
   let datas = await my_module.create_datas(items);
   const nowDate = new Date();
 
+  //テックさん
   await my_slack.send_message(my_module.dates_filter(datas, nowDate));
+  //my_lab
+  await my_slack.send_message2(my_module.dates_filter(datas, nowDate));
 
   //クラウドワークス 新着 アプリ・スマートフォン開発
   await page.goto('https://crowdworks.jp/public/jobs/search?category_id=242&keep_search_criteria=true&order=new&hide_expired=true');
@@ -23,7 +26,10 @@ const my_slack = require('./slack');
   items = await page.$$('.item_body');
   datas = await my_module.create_datas(items);
 
+  //テックさん
   await my_slack.send_message(my_module.dates_filter(datas, nowDate));
+  //my_lab
+  await my_slack.send_message2(my_module.dates_filter(datas, nowDate));
 
   await browser.close();
 })();
